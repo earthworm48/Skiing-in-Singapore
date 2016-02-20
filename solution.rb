c851842e3ee29afc5dc74d58f3b37e271e63ce76
earthworm48 <samkahchiin@gmail.com>
2016-02-20 21:26:46 +0800

    slight change of name

--- solution.rb
+++ solution.rb
@@ -1,21 +1,20 @@
 require 'byebug'
 
-array = []
+mountain = []
 
-File.open("simple.txt",'r') do |f|
+File.open("test.txt",'r') do |f|
 	f.each_line do |line|
-		array << line.split(" ").map(&:to_i)
+		mountain << line.split(" ").map(&:to_i)
 	end
 end
 
-dim = array.shift
-
+dim = mountain.shift
 possible_paths = []
 
 # Find every possible routes
 # Storing every possible initial point
 # Change it to normal coordinates point
-array.each_with_index do |row,y|
+mountain.each_with_index do |row,y|
 	row.each_with_index do |element,x|
 		possible_paths << [[x,y]]
 	end
@@ -27,60 +26,60 @@ possible_paths.each do |path_array|
 	x = path_array.last[0] 
 	y = path_array.last[1] 
 
-
-	coord = array[y][x]
-	# byebug
+	coord = mountain[y][x]
 	# Compare the point to their neightbouring point
 	# & compare with different direction
+
 	# Compare north
-	if y > 0 && array[y-1][x] < coord		
+	if y > 0 && mountain[y-1][x] < coord		
 		new_path = path_array.dup
 		new_path << [x,y-1]
-		# Store it into an array
+		# Append it into the array
 		possible_paths << new_path		
 	end
+
 	# Compare east
-	if x < dim[0] - 1 && array[y][x+1] < coord	
+	if x < dim[0] - 1 && mountain[y][x+1] < coord	
 		new_path = path_array.dup
 		new_path << [x+1,y]
 		possible_paths << new_path	
 	end
+
 	# Compare south
-	if y < dim[1] - 1 && array[y+1][x] < coord
+	if y < dim[1] - 1 && mountain[y+1][x] < coord
 		new_path = path_array.dup
 		new_path << [x,y+1]
 		possible_paths << new_path	
 	end
+
 	# Compare west
-	if x > 0 && array[y][x-1] < coord	
+	if x > 0 && mountain[y][x-1] < coord	
 		new_path = path_array.dup
 		new_path << [x-1,y]
 		possible_paths << new_path
 	end
-
 end
-
 # To find the max length of the array
 max_length = possible_paths.max_by{|a| a.length}
-array_with_max_length = possible_paths.find_all{|x| x.length == max_length.length}
+arrays_with_max_length = possible_paths.find_all{|x| x.length == max_length.length}
 
-big_elevation_arr = []
+all_elevation_arr = []
 
 # Return the elevation instead of coordinates
-array_with_max_length.each do |arr|
+arrays_with_max_length.each do |arr|
 	elevation_arr = []
 	arr.each do |coord|
 		x = coord[0]
 		y = coord[1]
 
-		elevation_arr << array[y][x]	
+		elevation_arr << mountain[y][x]	
 	end
-	big_elevation_arr << elevation_arr
+	all_elevation_arr << elevation_arr
 end
 
 # Compare the drop
-ans = big_elevation_arr.max_by do |a|
+ans = all_elevation_arr.max_by do |a|
 	a.first - a.last
 end
 
-puts "The longest path is the path of #{ans} with the length of #{ans.length}"
+puts "The longest path is the path of #{ans} with the length of #{ans.length} & the drop of #{ans.first - ans.last}"
