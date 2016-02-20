ad45ca75f440dd3e90d627ecc1e6e428b29b6b9f
earthworm48 <samkahchiin@gmail.com>
2016-02-20 20:45:05 +0800

    find the array with max length

--- solution.rb
+++ solution.rb
@@ -10,24 +10,74 @@ end
 
 dim = array.shift
 
-possible_routes = []
-route = []
-
+possible_paths = []
 
 # Find every possible routes
 # Storing every possible initial point
+# Change it to normal coordinates point
 array.each_with_index do |row,y|
 	row.each_with_index do |element,x|
-		possible_routes << [x,y]
+		possible_paths << [[x,y]]
 	end
 end
+count = 0
+possible_paths.each do |path_array|
+	
+	# Always take the last coordinates point ...
+	x = path_array.last[0] 
+	y = path_array.last[1] 
+
+
+	coord = array[y][x]
+	# byebug
+	# Compare the point to their neightbouring point
+	# & compare with different direction
+	# Compare north
+	if y > 0 && !array[y-1][x].nil?
+		if array[y-1][x] < coord
+			new_path = path_array.dup
+			new_path << [x,y-1]
+			# Store it into an array
+			possible_paths << new_path
+		end
+	end
+	# Compare east
+	if x < dim[0] - 1 && !array[y][x+1].nil?
+		if array[y][x+1] < coord
+			new_path = path_array.dup
+			new_path << [x+1,y]
+			possible_paths << new_path
+		end
+	end
+	# Compare south
+	if y < dim[1] - 1 && !array[y+1][x].nil?
+		if array[y+1][x] < coord
+			new_path = path_array.dup
+			new_path << [x,y+1]
+			possible_paths << new_path
+		end
+	end
+	# Compare west
+	if x > 0 && !array[y][x-1].nil?
+		if array[y][x-1] < coord
+			new_path = path_array.dup
+			new_path << [x-1,y]
+			possible_paths << new_path
+		end
+	end
+	count += 1
+
+end
+
+# To find the max length of the array
+max_length = possible_paths.max_by{|a| a.length}
+array_with_max_length = possible_paths.find_all{|x| x.length == max_length.length}
+
+#p max_routes = possible_path.max
 
 
 
 
-# Compare the point to their neightbouring point
 
-
-# Store it into an array
 # Compare the length of the possible routes
 # Compare the max drop
