ef47ba83917c9ee1a5565cf6d55baa014c350ee4
earthworm48 <samkahchiin@gmail.com>
2016-02-20 20:48:26 +0800

    refactor the code for comparing

--- solution.rb
+++ solution.rb
@@ -20,7 +20,7 @@ array.each_with_index do |row,y|
 		possible_paths << [[x,y]]
 	end
 end
-count = 0
+
 possible_paths.each do |path_array|
 	
 	# Always take the last coordinates point ...
@@ -33,51 +33,35 @@ possible_paths.each do |path_array|
 	# Compare the point to their neightbouring point
 	# & compare with different direction
 	# Compare north
-	if y > 0 && !array[y-1][x].nil?
-		if array[y-1][x] < coord
-			new_path = path_array.dup
-			new_path << [x,y-1]
-			# Store it into an array
-			possible_paths << new_path
-		end
+	if y > 0 && array[y-1][x] < coor		
+		new_path = path_array.dup
+		new_path << [x,y-1]
+		# Store it into an array
+		possible_paths << new_pat		
 	end
 	# Compare east
-	if x < dim[0] - 1 && !array[y][x+1].nil?
-		if array[y][x+1] < coord
-			new_path = path_array.dup
-			new_path << [x+1,y]
-			possible_paths << new_path
-		end
+	if x < dim[0] - 1 && array[y][x+1] < coord	
+		new_path = path_array.dup
+		new_path << [x+1,y]
+		possible_paths << new_path	
 	end
 	# Compare south
-	if y < dim[1] - 1 && !array[y+1][x].nil?
-		if array[y+1][x] < coord
-			new_path = path_array.dup
-			new_path << [x,y+1]
-			possible_paths << new_path
-		end
+	if y < dim[1] - 1 && array[y+1][x] < coord
+		new_path = path_array.dup
+		new_path << [x,y+1]
+		possible_paths << new_path	
 	end
 	# Compare west
-	if x > 0 && !array[y][x-1].nil?
-		if array[y][x-1] < coord
-			new_path = path_array.dup
-			new_path << [x-1,y]
-			possible_paths << new_path
-		end
+	if x > 0 && array[y][x-1] < coord	
+		new_path = path_array.dup
+		new_path << [x-1,y]
+		possible_paths << new_path
 	end
-	count += 1
 
 end
 
 # To find the max length of the array
 max_length = possible_paths.max_by{|a| a.length}
-array_with_max_length = possible_paths.find_all{|x| x.length == max_length.length}
+p array_with_max_length = possible_paths.find_all{|x| x.length == max_length.length}
 
-#p max_routes = possible_path.max
-
-
-
-
-
-# Compare the length of the possible routes
 # Compare the max drop
