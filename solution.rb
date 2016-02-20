127226185ece22e114133428e5482e3df8b6a439
earthworm48 <samkahchiin@gmail.com>
2016-02-20 15:43:08 +0800

    Storing initial point

--- solution.rb
+++ solution.rb
@@ -1,7 +1,33 @@
+require 'byebug'
+
 array = []
 
 File.open("simple.txt",'r') do |f|
 	f.each_line do |line|
-		array << line.split(" ")
+		array << line.split(" ").map(&:to_i)
 	end
 end
+
+dim = array.shift
+
+possible_routes = []
+route = []
+
+
+# Find every possible routes
+# Storing every possible initial point
+array.each_with_index do |row,y|
+	row.each_with_index do |element,x|
+		possible_routes << [x,y]
+	end
+end
+
+
+
+
+# Compare the point to their neightbouring point
+
+
+# Store it into an array
+# Compare the length of the possible routes
+# Compare the max drop
